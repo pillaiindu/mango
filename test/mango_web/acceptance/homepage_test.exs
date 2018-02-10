6bcaa053c2cd84df25e4a12bdef83c1a56d03966
Indu Pillai <pillaiindu@yahoo.com>
2018-02-10 17:48:00 +0500

    End adding seasonal products to homepage, Start 4.3 (View categorized products) page#110

--- test/mango_web/acceptance/homepage_test.exs
+++ test/mango_web/acceptance/homepage_test.exs
@@ -4,8 +4,18 @@ defmodule MangoWeb.HomepageTest do
 
   hound_session()
 
-  test "presence of featured products" do
+  test "presence of seasonal products" do
     navigate_to("/")
-    assert page_source() =~ "Seasonal products"
+
+    page_title = find_element(:css, ".page-title") |> visible_text()
+    assert page_title == "Seasonal products"
+
+    product = find_element(:css, ".product")
+    product_name = find_within_element(product, :css, ".product-name") |> visible_text()
+    product_price = find_within_element(product, :css, ".product-price") |> visible_text()
+
+    assert product_name == "Apple"
+    assert product_price == "100"
+    refute page_source() =~ "Tomato"
   end
 end
