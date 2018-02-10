7509b3df06152983dd63fe3a3280aa8fe8db9230
Indu Pillai <pillaiindu@yahoo.com>
2018-02-11 01:57:22 +0500

    End categorized products, start 4.4 (Adding Ecto) page#121

---
+++ test/mango_web/acceptance/category_page_test.exs
@@ -0,0 +1,44 @@
+defmodule MangoWeb.Acceptance.CategoryPageTest do
+  use ExUnit.Case
+  use Hound.Helpers
+
+  hound_session()
+
+  setup do
+    ## Given ##
+    # There are two products Apple and Tomato priced 100 and 50
+    # categorized under `fruits` and `vegetables` respectively
+    :ok
+  end
+
+  test "show fruits" do
+    navigate_to("/categories/fruits")
+    
+    page_title = find_element(:css, ".page-title") |> visible_text()
+    assert page_title == "Fruits"
+
+    product = find_element(:css, ".product")
+    product_name = find_within_element(product, :css, ".product-name") |> visible_text()
+    product_price = find_within_element(product, :css, ".product-price") |> visible_text()
+
+    assert product_name == "Apple"
+    assert product_price == "100"
+    refute page_source() =~ "Tomato"
+  end
+
+  test "show vegetables" do
+    navigate_to("/categories/vegetables")
+
+    page_title = find_element(:css, ".page-title") |> visible_text()
+    assert page_title == "Vegetables"
+
+    product = find_element(:css, ".product")
+    product_name = find_within_element(product, :css, ".product-name") |> visible_text()
+    product_price = find_within_element(product, :css, ".product-price") |> visible_text()
+
+    assert product_name == "Tomato"
+    assert product_price == "50"
+    refute page_source() =~ "Apple"
+  end
+  
+end
