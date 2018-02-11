148d6000436c4867f3d20259d0ebcb656ec42c7f
Indu Pillai <pillaiindu@yahoo.com>
2018-02-11 07:22:09 +0500

    end creating products, start 4.5 (Seed data) page#130

--- test/mango_web/acceptance/homepage_test.exs
+++ test/mango_web/acceptance/homepage_test.exs
@@ -1,8 +1,16 @@
 defmodule MangoWeb.HomepageTest do
-  use ExUnit.Case
+  use Mango.DataCase
   use Hound.Helpers
 
   hound_session()
+
+  setup do
+    alias Mango.Repo
+    alias Mango.Catalog.Product
+    Repo.insert %Product{name: "Tomato", price: 50, is_seasonal: false}
+    Repo.insert %Product{name: "Apple", price: 100, is_seasonal: true}
+    :ok
+  end
 
   test "presence of seasonal products" do
     navigate_to("/")
