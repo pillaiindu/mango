fa05366e7f2cce071794f74daf747f40183b00e6
Indu Pillai <pillaiindu@yahoo.com>
2018-02-14 04:23:53 +0500

    Finish Registration, Start 5.3 (Login), page#176

--- lib/mango_web/controllers/registration_controller.ex
+++ lib/mango_web/controllers/registration_controller.ex
@@ -4,18 +4,20 @@ defmodule MangoWeb.RegistrationController do
 
   def new(conn, _) do
     changeset = CRM.build_customer()
-    render(conn, "new.html", changeset: changeset)
+    residence_areas = Auroville.ResidenceService.list_areas
+    render(conn, "new.html", changeset: changeset, residence_areas: residence_areas)
   end
 
   def create(conn, %{"registration" => registration_params}) do
     case CRM.create_customer(registration_params) do
-      {:ok, customer} ->
+      {:ok, _customer} ->
         conn
         |> put_flash(:info, "Registration successful")
         |> redirect(to: page_path(conn, :index))
       {:error, changeset} ->
+        residence_areas = Auroville.ResidenceService.list_areas
         conn
-        |> render(:new, changeset: changeset)
+        |> render(:new, changeset: changeset, residence_areas: residence_areas)
     end
   end
 end
